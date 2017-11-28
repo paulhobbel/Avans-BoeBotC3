45195f4905727a00bdba0a346719f3978e63b6aa
Nick Kerremans <nickkerremans@gmail.com>
2017-11-28 15:09:35 +0100

    Fixed the JavaDoc for the Transmission class

--- boebot/Transmission.java
+++ boebot/Transmission.java
@@ -14,43 +14,89 @@ public class Transmission extends Updatable
     private final double DEGREETIME = 5.7; //time it take for the BoeBot to turn 1 degree at 1600 speed
     private Engine engineR = new Engine(15, false);
     private Engine engineL = new Engine(14, true);
-
+    /**
+     * Makes the servo's go straight forwards.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void forwards(Speed speed) {
         this.speed(speed.getSpeed(), speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's move backwards.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void backwards(Speed speed) {
         this.speed(-speed.getSpeed(), speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's brake from the current speed to zero.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void brake(Speed speed) {
         this.emergencyBrake(speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's move to the right with a given speed.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void right(Speed speed) {
         this.turnSpeed(speed.getSpeed(), speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's move to the left with a given speed.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void left(Speed speed) {
         this.turnSpeed(-speed.getSpeed(), speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's move to the right with a given speed and also makes a curve.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void curveRight(Speed speed) {
         this.curve(speed.getSpeedFast(), speed.getSpeedSlow(), speed.getAcceleration());
     }
     
+    /**
+     * Makes the servo's move to the left with a given speed and also makes a curve.
+     * 
+     * @see #Speed for more info.
+     * @param speed The speed constant.
+     */
     public void curveLeft(Speed speed) {
         this.curve(speed.getSpeedSlow(), speed.getSpeedFast(), speed.getAcceleration());
     }
     
+    /**
+     * Updates the right and left engines.
+     */
     public void update() {
         this.engineR.update();
         this.engineL.update();
     }
     
     /**
-     * Immediately drive at the desired speed
-     * @param   speed   the speed between -100 and 100 (0 is standing still)
+     * Drive to the given speed in the given amount of time.
+     * 
+     * @param speed The speed between -100 and 100 (0 is standing still).
+     * @param time Time given by the user. In this amount of time the servo's must reach the target speed.
      */
     private void speed(int speed, int time)
     {
@@ -74,8 +120,10 @@ public class Transmission extends Updatable
     }
 
     /**
-     * Immediately turn to the desired speed around the center point of the axis
-     * @param   speed   the speed between -100 (counter clockwise) and 100 (clockwise). 0 is standing still
+     * Turn to the desired speed around the center point of the axis in a given amount of time.
+     * 
+     * @param speed The speed between -100 (counter clockwise) and 100 (clockwise). 0 is standing still.
+     * @param time Time given by the user. In this amount of time the servo's must reach the target speed. 
      */
     private void turnSpeed(int speed, int time)
     {
@@ -83,6 +131,13 @@ public class Transmission extends Updatable
         engineL.setSpeed(-speed, time);
     }
     
+    /**
+     * Make a curve with a given speed in a given amount of time.
+     * 
+     * @param speedL Set the speed of the left servo.
+     * @param speedR Set the speed of the right servo.
+     * @param time Time given by the user. In this amount of time the servo's must reach the target speed.
+     */
     private void curve(int speedL, int speedR, int time)
     {
         engineL.setSpeed(speedL, time);
@@ -90,7 +145,9 @@ public class Transmission extends Updatable
     }
 
     /**
-     * Immediately stops the 2 engines
+     * Stops the engines in a given amount of time.
+     * 
+     * @param time Time given by the user. In this amount of time the servo's must reach the target speed.
      */
     private void emergencyBrake(int time)
     {
@@ -98,6 +155,9 @@ public class Transmission extends Updatable
         engineL.setSpeed(0, time);
     }
 
+    /**
+     * Class to store speed constants.
+     */
     public enum Speed
     {
         FAST(100, 500),
@@ -113,29 +173,54 @@ public class Transmission extends Updatable
         private int speedFast;
         private int acceleration;
         
+        /**
+         * The given speed by a user which should be reached with a constant acceleration.
+         *
+         * @param speed The speed between -100 (counter clockwise) and 100 (clockwise). 0 is standing still.
+         * @param acceleration Acceleration given by the user, which should be constant untill the right speed is reached.
+         */
         Speed(int speed, int acceleration) {
             this.speed = speed;
             this.acceleration = acceleration;
         }
         
+        /**
+         * The given speed by a user which should be reached with a constant acceleration.
+         *
+         * @param speedSlow Either the left or right wheel moves slow, depending on the method call.
+         * @param speedFast Either the left or right wheel moves fast, depending on the method call.
+         * @param acceleration Acceleration given by the user, which should be constant untill the right speed is reached.
+         */
         Speed(int speedSlow, int speedFast, int acceleration) {
             this.speedSlow = speedSlow;
             this.speedFast = speedFast;
             this.acceleration = acceleration;
         }
         
+        /**
+         * Get the current speed from the servo's.
+         */
         public int getSpeed() {
             return this.speed;
         }
         
+        /**
+         * Get the current slow speed from the servo's.
+         */
         public int getSpeedSlow() {
             return this.speedSlow;
         }
         
+        /**
+         * Get the current fast speed from the servo's.
+         */
         public int getSpeedFast() {
             return this.speedFast;
         }
         
+        /**
+         * Get the current acceleration from the servo's.
+         */
         public int getAcceleration() {
             return this.acceleration;
         }

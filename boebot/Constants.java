7391de9cfc5fbba0d9bae79430ff1c00dabbe6a8
Daan <daan.vankempen2@gmail.com>
2017-12-06 12:38:59 +0100

    Àdded constants for the line follower

--- boebot/Constants.java
+++ boebot/Constants.java
@@ -12,12 +12,14 @@ public class Constants
      */
     public static final int SERVO_RIGHT_PIN = 15;
     
+    //----------------------------------------------------\\
     
     /**
      * The pin of the remote IR.
      */
     public static final int REMOTE_PIN = 0;
     
+    //----------------------------------------------------\\
     
     /**
      * The pin of the ultrasone trigger
@@ -33,4 +35,58 @@ public class Constants
      * The distance which is needed to switch to CollisionState
      */
     public static final int COLLISION_DISTANCE = 15;
+    
+    //----------------------------------------------------\\
+    
+    /**
+     * The analog pin of the right light sensor
+     */
+    public static final int LIGHT_RIGHT_PIN = 0;
+    
+    /**
+     * The analog pin of the middle light sensor
+     */
+    public static final int LIGHT_MIDDLE_PIN = 1;
+    
+    /**
+     * The analog pin of the left light sensor
+     */
+    public static final int LIGHT_LEFT_PIN = 2;
+    
+    //----------------------------------------------------\\
+    
+    /**
+     * Linefollower constant: How much it corrects right away for an error
+     */
+    public static final double KP = 0.075;
+    
+    /**
+     * Linefollower constant: Prevents overreaction over small errors
+     */
+    public static final double KI = 0.002;
+    
+    /**
+     * Linefollower constant: How much it will react when it tries to predict the next error 
+     */
+    public static final double KD = 0.02;
+    
+    /**
+     * Linefollower constant: The base speed moving in a straight line
+     */
+    public static final int TP = 70;
+    
+    /**
+     * Linefollower constant: dampning the integral
+     */
+    public static final double DAMP = 0.8;
+    
+    /**
+     * Light value of a white surface
+     */
+    public static final int WHITE = 3900;
+    
+    /**
+     * Light value of a black surface
+     */
+    public static final int BLACK = 2400;
 }

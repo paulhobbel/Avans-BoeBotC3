57a2b117883845417d378bc08b2b3ed7a0f4b6f4
Bou <bpgroene@avans.nl>
2017-12-06 11:29:26 +0100

    adding jssc to the bluetooth class

--- boebot/hardware/Bluetooth.java
+++ boebot/hardware/Bluetooth.java
@@ -1,12 +1,37 @@
 package boebot.hardware;
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
 /**
  * Write a description of class Bluetooth here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Paul Hobbel
+ * @author Thomas Mandemaker
+ * @author Daan van Kempen
+ * @author Tim de Booij
+ * @author Nick Kerremans
+ * @author Boudewijn Groeneboer
+ * @version 06-12-2017
  */
 public class Bluetooth
 {
-    
+    public void signal(){
+        SerialPort serialPort = new SerialPort("COM4");
+        try {
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+            serialPort.writeString("HelloWorld");
+            
+            byte[] buffer =serialPort.readBytes(10);
+            for (int count = 0; count < 10; count++) 
+                System.out.print(buffer[count] + "-");
+            
+            serialPort.closePort();           
+        }
+        
+        catch (SerialPortException  ex) {
+            System.out.println(ex);
+        }
+    } 
 }

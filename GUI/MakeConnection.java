0726960b1782fed6cbf3a567de1cfc4507c61028
ThomasMandemaker <thomas.mandemaker@hotmail.com>
2017-12-08 12:10:35 +0100

    Added logs and a swing GUI

--- GUI/MakeConnection.java
+++ GUI/MakeConnection.java
@@ -1,8 +1,10 @@
 import jssc.*;
+import jssc.SerialPort.*;
+import java.io.*;
 
 public class MakeConnection{
     private SerialPort serialPort;
-
+    String initial = null;
     public MakeConnection()
     {
         serialPort = new SerialPort("COM5");
@@ -14,19 +16,35 @@ public class MakeConnection{
         try{
             serialPort.openPort();
             serialPort.setParams(115200, 8, 1, 0);
-
+            serialPort.addEventListener(new SerialPortEventListener(){
+                    public void serialEvent(SerialPortEvent event){
+                        TextClassStuff.addLog(test());
+                    }
+                });
         }
         catch(SerialPortException ex){
             System.out.println(ex);
         }
     }
 
-    public void sendInt(int i){
+    public String test(){
+        try{
+            byte[] buffer = serialPort.readBytes();
+            initial = serialPort.readString();
+            if(!(initial == null))
+                for(int count = 0; count < initial.length(); count++)
+                    System.out.print(buffer[count]);           
+        }catch(SerialPortException ex){
+            System.out.println(ex);
+        }
+        return initial;
+    }
+
+    public void sendString(String i){
         if(!serialPort.isOpened())
             return;
         try{
-            serialPort.writeInt(i);
-            
+            serialPort.writeString(i);
             //byte buffer[] = serialPort.readBytes(i.length());
 
             //for(int count = 0; count < s.length(); count++)
